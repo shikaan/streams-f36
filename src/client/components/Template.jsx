e66bc765fefa5ec2244f7210e4de9d871bb89045
shikaan <spagnolo.manu@gmail.com>
2019-12-07 12:41:08 +0100

    feat(client): use minimalistic Template

---
+++ src/client/components/Template.jsx
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+import Dashboard from "./Dashboard";
+
+const Template = () => (
+  <Fragment>
+    {/* Top Bar*/}
+    <Dashboard/>
+  </Fragment>
+);
+
+export default Template;
