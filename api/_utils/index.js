3e9172067c0c338e119a9e3bffaa0960c162d0cd
shikaan <spagnolo.manu@gmail.com>
2019-12-11 14:55:02 +0100

    refactor(server): create a route-matcher object
    And add a decent logger

--- api/_utils/index.js
+++ api/_utils/index.js
@@ -2,12 +2,16 @@ const {getThrowException} = require('./exception.handler');
 const {getSuccessHandler} = require("./success.handler");
 const bodyParser = require("./body-parser");
 const cookieParser = require("./cookie-parser");
-const {HTTP_METHOD} = require('./http-method')
+const {HTTP_METHOD} = require('./http-method');
+const {cors} = require('./cors.handler');
+const {RouteMatcher} = require('./route-matcher');
 
 module.exports = {
   bodyParser,
   cookieParser,
   getSuccessHandler,
   getThrowException,
-  HTTP_METHOD
+  HTTP_METHOD,
+  cors,
+  RouteMatcher
 };
--- api/server.js
+++ api/server.js
@@ -1,64 +1,35 @@
 const http = require('http');
+const debug = require('debug')('stream-f36');
 
 const self = require('./self');
 const session = require('./session');
 const stats = require('./stats');
 
 const {config} = require('./_config');
-const {HTTP_METHOD, getThrowException} = require('./_utils');
-const {NotFoundException, MethodNotAllowedException} = require("./_exceptions");
+const {HTTP_METHOD, getThrowException, cors, RouteMatcher} = require('./_utils');
 
-const getMatchRoute = (request, response) => {
-  const sendError = getThrowException(request, response);
-  let matched = false;
+const server = http.createServer(async (req, res) => {
+  const routeMatcher = new RouteMatcher(req, res, debug);
+  const sendError = getThrowException(req, res);
 
-  return (matcher, options, handlers) => {
-    const cleanUrl = request.url.replace('//', '/');
-    const handler = handlers[request.method];
-    const methods = Object.keys(handlers);
+  cors({allowed: true})(req, res);
 
-    if (matched) {
-      return;
-    }
+  try {
+    await routeMatcher.match('/api/stats', {exact: true}, {
+      [HTTP_METHOD.GET]: stats
+    });
 
-    if (!methods.includes(request.method)) {
-      return sendError(new MethodNotAllowedException())
-    }
+    await routeMatcher.match('/api/session', {exact: true}, {
+      [HTTP_METHOD.POST]: session
+    });
 
-    if (!handler) {
-      return sendError(new NotFoundException())
-    }
+    await routeMatcher.match('/api/self', {exact: true}, {
+      [HTTP_METHOD.GET]: self
+    })
+  } catch (e) {
+    sendError(e)
+  }
 
-    if (options.exact) {
-      if (cleanUrl === matcher) {
-        matched = true;
-        return handler(request, response)
-      }
-    } else {
-      const regExpMather = new RegExp(matcher);
-
-      if (regExpMather.test(cleanUrl)) {
-        matched = true;
-        return handler(request, response)
-      }
-    }
-  };
-};
-
-const server = http.createServer((req, res) => {
-  const matchRoute = getMatchRoute(req, res);
-
-  matchRoute('/api/stats', {exact: true}, {
-    [HTTP_METHOD.GET]: stats
-  });
-
-  matchRoute('/api/session', {exact: true}, {
-    [HTTP_METHOD.POST]: session
-  });
-
-  matchRoute('/api/self', {exact: true}, {
-    [HTTP_METHOD.GET]: self
-  })
 });
 
 
